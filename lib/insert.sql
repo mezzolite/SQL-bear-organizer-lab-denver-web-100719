6859a170debfae34f86b456743b90e50e2445512
mezzolite <mez.charney@gmail.com>
2019-10-18 11:19:23 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive
+) VALUES (
+    "Mr. Chocolate", 4, "Other", "brown", "sweet", 1
+);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive
+) VALUES (
+    "Rowdy", 4, "Male", "brown", "rowdy", 0
+);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive
+) VALUES (
+    "Tabitha", 3, "Female", "red", "strange", 1
+);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive
+) VALUES (
+    "Sergeant Brown", 7, "Female", "black", "territorial", 1
+);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive
+) VALUES (
+    "Melissa", 3, "Other", "brown", "playful", 1
+);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive
+) VALUES (
+    "Grinch", 9, "Male", "black", "silly", 0
+);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive
+) VALUES (
+    "Wendy", 4, "Female", "blonde", "tidy", 1
+);
+
+INSERT INTO bears (
+    age, gender, color, temperament, alive
+) VALUES (
+     6, "Other", "brown", "hungry", 0
+);
